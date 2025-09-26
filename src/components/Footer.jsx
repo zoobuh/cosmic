e76560c45dc09db39602cf6bd14a239d0eeb56f2
wiltedflower <91960533+stellarcase@users.noreply.github.com>
2025-09-26 19:32:55 -0400

    don't hide entire footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,6 @@ import clsx from 'clsx';
 const Footer = () => {
   const { options } = useOptions();
   return (
-    options.donationBtn !== false && (
       <div className="w-full fixed bottom-0 flex items-end justify-between p-2">
         <div
           className={clsx(
@@ -17,15 +16,16 @@ const Footer = () => {
           <Disc className="w-4" fill={options.siteTextColor || "#a0b0c8"} />
           Discord
         </div>
-        <a href="https://www.buymeacoffee.com/dogubdev" target="_blank" rel="noopener noreferrer">
-          <img
-            src="https://img.buymeacoffee.com/button-api/?text=Support us&emoji=☕&slug=dogubdev&button_colour=40DCA5&font_colour=ffffff&font_family=Inter&outline_colour=000000&coffee_colour=FFDD00"
-            alt="Support us"
-            className="w-32 h-auto"
-          />
-        </a>
+        {options.donationBtn !== false && (
+          <a href="https://www.buymeacoffee.com/dogubdev" target="_blank" rel="noopener noreferrer">
+            <img
+              src="https://img.buymeacoffee.com/button-api/?text=Support us&emoji=☕&slug=dogubdev&button_colour=40DCA5&font_colour=ffffff&font_family=Inter&outline_colour=000000&coffee_colour=FFDD00"
+              alt="Support us"
+              className="w-32 h-auto"
+            />
+          </a>
+        )}
       </div>
-    )
   );
 };
 
